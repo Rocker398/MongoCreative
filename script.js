45a37a0044908a047bee7ce5d0199adce0c9ae2d
Paden Fausett <paden19@hotmail.com>
2017-10-17 03:15:34 +0000

    Added initial work on project

---
+++ script.js
@@ -0,0 +1,92 @@
+angular.module('ImageCorkboard', [])
+  .controller('MainCtrl', mainCtrl)
+  .directive('imageCork', imageDirective);
+
+function mainCtrl ($scope) {
+	
+	$scope.images = [];
+	$scope.width = 30;
+	
+	$scope.addImage = function (image) {
+		$scope.images.push({ 
+			Url: image.Url
+		});
+		
+		image.Url = '';
+		
+		setTimeout(triggerSlider, 0.1);
+	};
+}
+
+function triggerSlider() {
+	$('input[type=range]').trigger('input');
+};
+
+function imageDirective () {
+	return {
+		scope: {
+			image: '=', /* [1] */
+			index: '@'
+		},
+		restrict: 'E', /* [2] */
+		replace: 'true',
+		template: (
+			'<div class="ImageCork" style="z-index:{{index}};">' +
+				'<div class="imageFrame">' +
+					'<img ng-src="{{image.Url}}" />' +
+				'</div>' +
+			'</div>'
+		), /* [3] */
+		link: link
+	};
+
+	function link (scope, elm) { /* [4] */
+		if (!scope.image.Url) {
+			scope.image.Url = 'placeholder.jpg';
+		}
+		elm.draggable({
+			cursor: "move",
+			containment: ".corkboardContainer"
+		});
+	}
+}
+
+$(document).ready(function() {
+	$('input[type=range]').wrap("<div class='range'></div>");
+	var i = 1;
+
+	$('.range').each(function() {
+		var n = this.getElementsByTagName('input')[0].value;
+		var x = (n / 50) * (this.getElementsByTagName('input')[0].offsetWidth - 8) - 12;
+		this.id = 'range' + i;
+		
+		var percent = (n / 50) * 100;
+		
+		if (this.getElementsByTagName('input')[0].value == 0) {
+			this.className = "range"
+		} 
+		else {
+			this.className = "range rangeM"
+		}
+		
+		this.innerHTML += "<style>#" + this.id + " input[type=range]::-webkit-slider-runnable-track {background:linear-gradient(to right, #3f51b5 0%, #3f51b5 " + percent + "%, #515151 " + percent + "%)} #" + this.id + ":hover input[type=range]:before{content:'" + n + "'!important;left: " + x + "px;} #" + this.id + ":hover input[type=range]:after{left: " + x + "px}</style>";
+		i++;
+	});
+
+	$('input[type=range]').on("input", function() {
+		var a = this.value;
+		var p = (a / 50) * (this.offsetWidth - 8) - 12;
+		var percent = (a / 50) * 100;
+		
+		if (a == 0) {
+			this.parentNode.className = "range"
+		} 
+		else {
+			this.parentNode.className = "range rangeM"
+		}
+		
+		this.parentNode.getElementsByTagName('style')[0].innerHTML += "#" + this.parentNode.id + " input[type=range]::-webkit-slider-runnable-track {background:linear-gradient(to right, #3f51b5 0%, #3f51b5 " + percent + "%, #515151 " + percent + "%)} #" + this.parentNode.id + ":hover input[type=range]:before{content:'" + a + "'!important;left: " + p + "px;} #" + this.parentNode.id + ":hover input[type=range]:after{left: " + p + "px}";
+		
+		$('.ImageCork').css('width', a + '%');
+	})
+});
