94a5fbe46afab09f43e004c653f30119557b0097
Paden Fausett <paden19@hotmail.com>
2017-11-14 15:57:24 +0000

    added get and post routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Pins = mongoose.model('Pins');
+
+router.get('/pins', function(req, res, next) {
+  Pins.find(function(err, pins) {
+    if(err) { return next(err); }
+    res.json(pins);
+  });
+}
+
+router.post('/pins', function(req, res, next) {
+  var pin = new Pin(req.body);
+  pin.save(function(err, pin) {
+    if(err) { return next(err); }
+    res.json(pin);
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
